45e5deba5740d5135cb9d21a437705af7ff160ce
Emilie Gachoud <emilie@gachoud.ch>
2017-02-28 09:35:07 +0100

    propriétés user

--- models/user.js
+++ models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-  name: String
+    firstName: String,
+    lastName: String,
+    role: String,
+    createdAt: Date
+    
 });
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
